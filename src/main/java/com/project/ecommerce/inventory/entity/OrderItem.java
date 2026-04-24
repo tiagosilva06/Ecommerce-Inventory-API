e919b953b70e5609d7305ffd5ccfecfcb2b00ca2
Tiago Santos <tiagosilva9278@gmail.com>
2026-04-24 17:33:28 -0300

    feat(order): implement fields and mappings in OrderItem entity

--- src/main/java/com/project/ecommerce/inventory/entity/OrderItem.java
+++ src/main/java/com/project/ecommerce/inventory/entity/OrderItem.java
@@ -1,4 +1,35 @@
 package com.project.ecommerce.inventory.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+import java.math.BigDecimal;
+
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@EqualsAndHashCode(of = "id")
+@Entity
+@Table(name = "order_items")
 public class OrderItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "quantity")
+    private int quantity;
+
+    @Column(name = "price")
+    private BigDecimal price;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private Order order;
 }
